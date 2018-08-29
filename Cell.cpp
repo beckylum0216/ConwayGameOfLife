4c10f656198a311b0d5cdafd47ffe7b760cba1eb
Rebecca <beckylum0214@gmail.com>
2018-08-29 22:06:17 +0800

    RL getters and setters

--- Cell.cpp
+++ Cell.cpp
@@ -23,25 +23,38 @@ Cell::~Cell()
 	*/
 }
 
+void Cell::SetPositionX(int inputX)
+{
+	positionX = inputX;
+}
 
+int Cell::GetPositionX()
+{
+	return positionX;
+}
 
+void Cell::SetPositionY(int inputY)
+{
+	positionY = inputY;
+}
 
 void Cell::SetCellState(bool inputState)
 {
 	cellState = inputState;
 }
 
-//Any live cell with fewer than two live neighbours dies(referred to as underpopulation or exposure[1]).
-//Any live cell with more than three live neighbours dies(referred to as overpopulation or overcrowding).
-//Any live cell with two or three live neighbours lives, unchanged, to the next generation.
-//Any dead cell with exactly three live neighbours will come to life.
-
-void Cell::SetCellState(bool inputState)
+bool Cell::GetCellState()
 {
-	cellState = inputState;
+	return cellState;
 }
+
 
 void Cell::SetNeighbourhoodCount(int inputCount)
 {
-	
+	neighbourCount = inputCount;
+}
+
+int Cell::GetNeighbourhoodCount()
+{
+	return neighbourCount;
 }
